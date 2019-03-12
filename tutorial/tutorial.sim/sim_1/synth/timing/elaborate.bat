0b7589aec07e8f72489ce5e8d32f94b3b1b29bb5
etonw <eton.williams@gmail.com>
2019-03-12 15:07:04 +0100

    back to 2017.2

--- tutorial/tutorial.sim/sim_1/synth/timing/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto 976f38af888f47d8b3b830b9c39d1c56 -m64 --debug typical --relax --mt 2 --maxdelay -L xil_defaultlib -L simprims_ver -L secureip --snapshot counter_tb_time_synth -transport_int_delays -pulse_r 0 -pulse_int_r 0 -pulse_e 0 -pulse_int_e 0 xil_defaultlib.counter_tb xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
